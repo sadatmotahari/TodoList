9ee2005c7736f2fb84394c68ff48f4dfabefa6f9
Zahra Sadat Motahari <sadatmotahari37@gmail.com>
2022-11-01 13:01:15 -0700

    new

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
-let button = document.getElementById("button");
+let addButton = document.getElementById("button");
 let input = document.getElementById("input");
 let ul = document.querySelector("#showList ul");
-function create(){
-    if(input.value !== ""){
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    let removeButton = document.createElement("button");
-    removeButton.appendChild(document.createTextNode("X"));
-    li.appendChild(removeButton);
-    removeButton.addEventListener("click" , function(){
-        li.remove();
-    })
-    li.addEventListener("click" , function(){
-        li.classList.toggle("active");
-    })
-    ul.appendChild(li);
-    input.value = "";
+let todoList = [];
+addButton.addEventListener("click" , addTodo);
+function addTodo(){
+    let todoText = input.value;
+    if(todoText == ""){
+        alert("لطفا فیلد را پر کنید");
+    } else {
+        let todoObject = {
+            subject: todoText,
+            isDone: false,
+        }
+        todoList.unshift(todoObject);
+        displayTodos();
+    }
+    input.value= "";
+} 
+function addTodoWithEnter(event){
+    if(event.keyCode === 13){
+        addTodo();
     }
 }
-button.addEventListener("click", create)
+input.addEventListener("keypress" , addTodoWithEnter);
+function displayTodos(){
+    ul.innerHTML="";
+    for(let num=0; num < todoList.length; num++){
+        let listElement = document.createElement("li");
+        let deleteBtn = document.createElement("button");
+        deleteBtn.setAttribute("data-id" , todoList[num].subject)
+        deleteBtn.appendChild(document.createTextNode("x"));
+        listElement.innerHTML = todoList[num].subject;
+        listElement.appendChild(deleteBtn)
+        if(todoList[num].isDone === true){
+            listElement.classList.add("active")
+        }
+        deleteBtn.addEventListener("click" , function(event){
+            let selectedItem = event.target.getAttribute("data-id")
+            deleteItem(selectedItem)
+        })
+        ul.append(listElement);
+    }
+}
+function deleteItem(selectedItem){
+   todoList.splice( todoList.findIndex(function(item){
+    if (item.subject == selectedItem){
+        return true
+    }
+   }) , 1)
+   displayTodos()
+}
+displayTodos();
